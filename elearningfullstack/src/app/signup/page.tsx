fb33d5aebe8b2d992d91580a91e7b60c12fb6139
Muzammil React(Developer) <110664405+Muzzammil387@users.noreply.github.com>
2023-11-02 22:27:55 +0500

    start signupform design

---
+++ elearningfullstack/src/app/signup/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <>
+    <div className='backShadow'></div>
+    <div className='SignUpModal grid'>
+      <div className="SignUpModal__left">
+        <div>
+          <img src={require("../dist/webImages/1.png")} alt="" />
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default SignUp
